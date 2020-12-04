b5ec08c4902eda700af49af9626646a93af48ff9
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-12-03 18:18:42 -0800

    2020 progress

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,26 +1,52 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day02 : ASolution
     {
-
-        public Day02() : base(02, 2020, "")
+        private static string[] Lines;
+        public Day02() : base(02, 2020, "Password Philosophy")
         {
-
+            Lines = Input.SplitByNewline(true);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var counterPassword = 0;
+            foreach (var line in Lines)
+            {
+                var segments = line.Split(new[] {'-', ' ', ':'});
+                var minOccurence = int.Parse(segments[0]);
+                var maxOccurence = int.Parse(segments[1]);
+                var charToCheck = segments[2][0];
+                var password = segments[^1];
+                var counterChar = 0;
+                foreach (var c in password)
+                {
+                    if (c == charToCheck) counterChar++;
+                }
+
+                if ((minOccurence <= counterChar) && (maxOccurence >= counterChar)) counterPassword++;
+            }
+            return counterPassword.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var counterPassword = 0;
+            foreach (var line in Lines)
+            {
+                var segments = line.Split(new[] {'-', ' ', ':'});
+                var firstPosition = int.Parse(segments[0]) - 1;
+                var secondPosition = int.Parse(segments[1]) - 1;
+                var charToCheck = segments[2][0];
+                var password = segments[^1];
+                if (
+                    ((password[firstPosition] == charToCheck) | (password[secondPosition] == charToCheck))
+                    && (password[firstPosition] != password[secondPosition])
+                    )
+                    counterPassword += 1;
+            }
+            return counterPassword.ToString();
         }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day03/Solution.cs
@@ -1,26 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day03 : ASolution
     {
-
-        public Day03() : base(03, 2020, "")
+        private static string[] _lines;
+        private static int _heightOfGrid;
+        private static int _widthOfGrid;
+        private const char TreeSymbol = '#';
+        private static readonly (int X, int Y) StartingPosition = (0, 0);
+        private static readonly (int deltaY, int deltaX)[] PossibleSlopes = new[]
         {
-
+            (1, 3), (1, 1), (1, 5), (1, 7), (2, 1)
+        };
+        
+        public Day03() : base(03, 2020, "Toboggan Trajectory")
+        {
+            _lines = Input.SplitByNewline();
+            _heightOfGrid = _lines.Length;
+            _widthOfGrid = _lines[0].Length;
         }
-
+        
         protected override string SolvePartOne()
         {
-            return null;
+            return CountTreesIntersectedBySlopeOf(PossibleSlopes[0]).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var treesEncountered = CountTreesIntersectedBySlopeOf(PossibleSlopes[0]);
+            foreach (var slope in PossibleSlopes[1..])
+            { 
+                treesEncountered *= CountTreesIntersectedBySlopeOf(slope);
+            }
+
+            return treesEncountered.ToString();
+        }
+
+        private static int CountTreesIntersectedBySlopeOf((int deltaY, int deltaX) slope)
+        {
+            var treesEncountered = 0;
+            var currentPosition = StartingPosition;
+            while (currentPosition.Y < _heightOfGrid)
+            {
+                if (_lines[currentPosition.Y][currentPosition.X % _widthOfGrid] == TreeSymbol) treesEncountered += 1;
+                currentPosition = (X: currentPosition.X + slope.deltaX, Y: currentPosition.Y + slope.deltaY);
+            }
+            
+            return treesEncountered;
         }
     }
 }
