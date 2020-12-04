996e21b083d2b5a76a0a895014c1b829e41877fc
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2020-12-03 22:10:57 -0800

    2020 D04 P1

--- AdventOfCode/Solutions/Year2020/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day04/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,15 +8,46 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day04 : ASolution
     {
+        private static List<List<string>> PassportLinesOfLines;
 
-        public Day04() : base(04, 2020, "")
+        private static readonly HashSet<string> RequiredFields =
+        new HashSet<string>() {
+            "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
+        };
+
+        private static readonly string[] OptionalFields =
         {
-
+            "cid"
+        };
+        
+        public Day04() : base(04, 2020, "Passport Processing")
+        {
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var lines = Input.Split(Environment.NewLine).ToList();
+            lines.Add(string.Empty);
+            var counterValidPassports = 0;
+            var passportFields = new List<string>();
+            foreach (var line in lines)
+            {
+                var distinctPassportFields = new HashSet<string>(passportFields);
+                if (line == string.Empty && distinctPassportFields.SetEquals(RequiredFields)) counterValidPassports++;
+                if (line == string.Empty)
+                {
+                    passportFields = new List<string>();
+                    continue;
+                }
+
+                foreach (var segment in line.Split(" "))
+                {
+                    var fieldName = segment.Split(':')[0];
+                    if (RequiredFields.Contains(fieldName)) passportFields.Add(segment.Split(':')[0]);
+                }
+            }
+
+            return counterValidPassports.ToString();
         }
 
         protected override string SolvePartTwo()
@@ -24,3 +56,4 @@ namespace AdventOfCode.Solutions.Year2020
         }
     }
 }
+
